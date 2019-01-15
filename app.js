497f7f8dae6c6ea32dae4aceeb3a9918876ce054
Melissa <melissapotter.student@careerdevs.com>
2019-01-15 03:00:34 +0000

    streams

--- app.js
+++ app.js
@@ -1,10 +1,20 @@
 var http = require ('http');
+var fs = require('fs');
 
-var server = http.createServer(function(req, res){
-    console.log('request was made: '+ req.url);
-    res.writeHead(200, {'Content-Type': 'text/plain'});
-    res.end('Hello Everyone');
+var myReadStream = fs.createReadStream(__dirname + '/README.txt', 'utf8');
+
+myReadStream.on('data', function(chunk){
+    console.log('new chunk receieved');
+    console.log(chunk);
 });
 
-server.listen(3000, '127.0.0.1');
-console.log('now listening to port 3000');
+
+
+// var server = http.createServer(function(req, res){
+//     console.log('request was made: '+ req.url);
+//     res.writeHead(200, {'Content-Type': 'text/plain'});
+//     res.end('Hello Everyone');
+// });
+
+// server.listen(3000, '127.0.0.1');
+// console.log('now listening to port 3000');
