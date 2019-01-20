f196db4ff788cbee960d6f8ba05929520b2677ac
Melissa <melissapotter.student@careerdevs.com>
2019-01-20 22:40:46 +0000

    pipes send info to writable streams

--- app.js
+++ app.js
@@ -1,21 +1,16 @@
 var http = require ('http');
 var fs = require('fs');
 
-var myReadStream = fs.createReadStream(__dirname + '/README.txt', 'utf8');
-var myWriteStream = fs.createWriteStream(__dirname + '/writeME.txt');
 
-myReadStream.on('data', function(chunk){
-    console.log('new chunk receieved');
-myWriteStream.write(chunk);
+
+
+
+var server = http.createServer(function(req, res){
+    console.log('request was made: '+ req.url);
+    res.writeHead(200, {'Content-Type': 'text/plain'});
+    var myReadStream = fs.createReadStream(__dirname + '/README.txt', 'utf8');
+    myReadStream.pipe(res);
 });
 
-
-
-// var server = http.createServer(function(req, res){
-//     console.log('request was made: '+ req.url);
-//     res.writeHead(200, {'Content-Type': 'text/plain'});
-//     res.end('Hello Everyone');
-// });
-
-// server.listen(3000, '127.0.0.1');
-// console.log('now listening to port 3000');
+server.listen(3000, '127.0.0.1');
+console.log('now listening to port 3000');
