d8f6186c62520b0d77a0543aa27acb818d8b447d
Melissa <melissapotter.student@careerdevs.com>
2019-01-20 23:35:10 +0000

    routing

--- app.js
+++ app.js
@@ -7,13 +7,22 @@ var fs = require('fs');
 
 var server = http.createServer(function(req, res){
     console.log('request was made: '+ req.url);
-    res.writeHead(200, {'Content-Type': 'application/json'});
-    var myObj = {
-        name: 'Mel',
-        job: 'Developer',
-        age: 25
-    };
-    res.end(JSON.stringify(myObj));
+    if(req.url === '/home' || req.url === '/'){
+        res.writeHead(200, {'Content-Type': 'text/html'});
+        fs.createReadStream(__dirname + '/index.html').pipe(res);
+    } else if(req.url === '/contact'){
+        res.writeHead(200, {'Content-Type': 'text/html'});
+        fs.createReadStream(__dirname + '/contact.html').pipe(res);
+    }
+    else if(req.url === '/api/coders'){
+        var coders = [{name: 'mel', age: 25}, {name: 'mario', age: 42}];
+        res.writeHead(200, {'Content-Type': 'application/json'});
+        res.end(JSON.stringify(coders));
+    } 
+    else{
+         res.writeHead(404, {'Content-Type': 'text/html'});
+        fs.createReadStream(__dirname + '/404.html').pipe(res);
+    }
 });
 
 server.listen(3000, '127.0.0.1');
