30f42001750d9593114dda19b5329bd28abb770e
Melissa <melissapotter.student@careerdevs.com>
2019-01-20 23:10:43 +0000

    serving JSON data

--- app.js
+++ app.js
@@ -7,9 +7,13 @@ var fs = require('fs');
 
 var server = http.createServer(function(req, res){
     console.log('request was made: '+ req.url);
-    res.writeHead(200, {'Content-Type': 'text/html'});
-    var myReadStream = fs.createReadStream(__dirname + '/index.html', 'utf8');
-    myReadStream.pipe(res);
+    res.writeHead(200, {'Content-Type': 'application/json'});
+    var myObj = {
+        name: 'Mel',
+        job: 'Developer',
+        age: 25
+    };
+    res.end(JSON.stringify(myObj));
 });
 
 server.listen(3000, '127.0.0.1');
