a3139468051ba0712fd912d7a7b4954f7bc2f8f6
Melissa <melissapotter.student@careerdevs.com>
2019-01-22 00:41:44 +0000

    express

--- app.js
+++ app.js
@@ -1,41 +1,42 @@
-var createError = require('http-errors');
+var http = require ('http');
+var fs = require('fs');
 var express = require('express');
-var path = require('path');
-var cookieParser = require('cookie-parser');
-var logger = require('morgan');
-
-var indexRouter = require('./routes/index');
-var usersRouter = require('./routes/users');
 
 var app = express();
 
-// view engine setup
-app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'jade');
 
-app.use(logger('dev'));
-app.use(express.json());
-app.use(express.urlencoded({ extended: false }));
-app.use(cookieParser());
-app.use(express.static(path.join(__dirname, 'public')));
-
-app.use('/', indexRouter);
-app.use('/users', usersRouter);
-
-// catch 404 and forward to error handler
-app.use(function(req, res, next) {
-  next(createError(404));
+app.get('/', function(req, res){
+  res.send('this is the homepage')
 });
 
-// error handler
-app.use(function(err, req, res, next) {
-  // set locals, only providing error in development
-  res.locals.message = err.message;
-  res.locals.error = req.app.get('env') === 'development' ? err : {};
-
-  // render the error page
-  res.status(err.status || 500);
-  res.render('error');
+app.get('/contact', function(req, res){
+  res.send('this is the contact page')
 });
 
-module.exports = app;
+app.listen(3000);
+
+
+
+
+// var server = http.createServer(function(req, res){
+//     console.log('request was made: '+ req.url);
+//     if(req.url === '/home' || req.url === '/'){
+//         res.writeHead(200, {'Content-Type': 'text/html'});
+//         fs.createReadStream(__dirname + '/index.html').pipe(res);
+//     } else if(req.url === '/contact'){
+//         res.writeHead(200, {'Content-Type': 'text/html'});
+//         fs.createReadStream(__dirname + '/contact.html').pipe(res);
+//     }
+//     else if(req.url === '/api/coders'){
+//         var coders = [{name: 'mel', age: 25}, {name: 'mario', age: 42}];
+//         res.writeHead(200, {'Content-Type': 'application/json'});
+//         res.end(JSON.stringify(coders));
+//     } 
+//     else{
+//         res.writeHead(404, {'Content-Type': 'text/html'});
+//         fs.createReadStream(__dirname + '/404.html').pipe(res);
+//     }
+// });
+
+// server.listen(3000, '127.0.0.1');
+// console.log('now listening to port 3000');
