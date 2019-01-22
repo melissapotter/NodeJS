8f4ba8511d151848f6e6a3b4256eb4b2a616e4d9
Melissa <melissapotter.student@careerdevs.com>
2019-01-22 01:07:33 +0000

    route params

--- app.js
+++ app.js
@@ -6,11 +6,15 @@ var app = express();
 
 
 app.get('/', function(req, res){
-  res.send('this is the homepage')
+  res.send('this is the homepage');
 });
 
 app.get('/contact', function(req, res){
-  res.send('this is the contact page')
+  res.send('this is the contact page');
+});
+
+app.get('/profile/:id', function(req, res){
+  res.send('You requested to see the ID of ' + req.params.id);
 });
 
 app.listen(3000);
